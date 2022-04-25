bb053d6bfec5c4e69fbb177bc2836eb5fa877ab6
Abhishek Singh <abhisheksinghxix@gmail.com>
2022-04-25 21:09:38 +0530

    Implement login

--- src/controllers/auth/login.ts
+++ src/controllers/auth/login.ts
@@ -1,4 +1,36 @@
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import {
+  BadRequestError,
+  NotFoundError,
+  UnauthorizedError,
+} from '../../errors';
+import { User } from '../../models';
+import {
+  attachTokenCookie,
+  createTokenUser,
+  sendVerificationMail,
+} from '../../utils';
+
 export const login = async (req: Request, res: Response) => {
-  res.json({ msg: 'login' });
+  const { email, password } = req.body;
+
+  if (!(email && password)) {
+    throw BadRequestError('Provide both email and password');
+  }
+  const user = await User.findOne({ email });
+
+  if (!user) throw NotFoundError(`No user found with email ${email}`);
+
+  if (!user.isVerified) {
+    sendVerificationMail(user);
+    throw UnauthorizedError('Please verify your email id before you log in.');
+  }
+
+  if (!(await user.comparePassword(password)))
+    throw UnauthorizedError('Wrong password');
+
+  const tokenUser = createTokenUser(user);
+  attachTokenCookie(res, tokenUser);
+  res.status(StatusCodes.OK).json({ user: tokenUser });
 };
