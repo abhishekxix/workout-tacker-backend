154097c7709029ab9cbc8c3f685bb6110734fe99
Abhishek Singh <abhisheksinghxix@gmail.com>
2022-04-30 21:26:37 +0530

    Implement updateUser route

--- src/controllers/user/updateUser.ts
+++ src/controllers/user/updateUser.ts
@@ -1,5 +1,21 @@
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import validator from 'validator';
+import { BadRequestError } from '../../errors';
+import { User } from '../../models';
+import { attachTokenCookie, createTokenUser } from '../../utils';
 
-export const updateUser = async (req: Request, res: Response) => {
-  res.json({ msg: 'updateUser' });
+export const updateUser = async (req: Request | any, res: Response) => {
+  const { name }: { name: string } = req.body;
+  if (!(name && validator.isAlpha(name)))
+    throw BadRequestError('Invalid value for name');
+
+  const user = await User.findById(req.user._id);
+  user.name = name;
+
+  const tokenUser = createTokenUser(user);
+  attachTokenCookie(res, tokenUser);
+  await user.save();
+
+  res.status(StatusCodes.OK).json({ user: tokenUser });
 };
