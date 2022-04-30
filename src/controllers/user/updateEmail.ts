24688046afbf00df3ab76dc04d849def9dc46709
Abhishek Singh <abhisheksinghxix@gmail.com>
2022-04-30 21:37:03 +0530

    Implement updateEmail route

--- src/controllers/user/updateEmail.ts
+++ src/controllers/user/updateEmail.ts
@@ -1,5 +1,25 @@
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { BadRequestError } from '../../errors';
+import { User } from '../../models';
+import { sendVerificationMail } from '../../utils';
 
-export const updateEmail = async (req: Request, res: Response) => {
-  res.json({ msg: 'updateEmail' });
+export const updateEmail = async (req: any, res: Response) => {
+  const { email } = req.body;
+  const { _id } = req.user;
+
+  if (!email) {
+    throw BadRequestError('Please provide a value for email');
+  }
+
+  const user = await User.findById(_id);
+  user.email = email;
+  user.isEmailVerified = false;
+  await user.save();
+
+  await sendVerificationMail(user);
+
+  res
+    .status(StatusCodes.ACCEPTED)
+    .json({ msg: 'Please verify your email address.' });
 };
