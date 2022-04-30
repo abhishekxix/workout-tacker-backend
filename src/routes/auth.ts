c749da215ce67963c5b10fcc5c162bc331266a53
Abhishek Singh <abhisheksinghxix@gmail.com>
2022-04-30 15:14:39 +0530

    Remove redundant imports and routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -5,9 +5,6 @@ import {
   logout,
   verifyEmail,
   verifyPhone,
-  resetPassword,
-  verifyAndResetPassword,
-  deleteAccount,
 } from '../controllers/auth';
 
 export const authRouter = express.Router();
@@ -17,8 +14,3 @@ authRouter.route('/login').post(login);
 authRouter.route('/logout').get(logout);
 authRouter.route('/verifyEmail/:verificationToken').get(verifyEmail);
 authRouter.route('/verifyPhone/:verificationToken').get(verifyPhone);
-authRouter.route('/resetPassword').get(resetPassword);
-authRouter
-  .route('/verifyAndResetPassword/:verificationToken')
-  .patch(verifyAndResetPassword);
-authRouter.route('/deleteAccount').delete(deleteAccount);
