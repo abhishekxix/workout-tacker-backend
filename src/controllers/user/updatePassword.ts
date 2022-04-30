b92f00c78c8cd2a5a42f332bd4e9024ca8c40352
Abhishek Singh <abhisheksinghxix@gmail.com>
2022-04-30 22:01:10 +0530

    Implement updatePassword route

--- src/controllers/user/updatePassword.ts
+++ src/controllers/user/updatePassword.ts
@@ -1,5 +1,31 @@
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import {
+  BadRequestError,
+  NotFoundError,
+  UnauthorizedError,
+} from '../../errors';
+import { User } from '../../models';
 
-export const updatePassword = async (req: Request, res: Response) => {
-  res.json({ msg: 'updatePassword' });
+export const updatePassword = async (req: any, res: Response) => {
+  const { oldPassword, newPassword } = req.body;
+  const { _id } = req.user;
+
+  if (!(oldPassword && newPassword)) {
+    throw BadRequestError('Insufficient information.');
+  }
+
+  const user = await User.findById(_id);
+
+  if (!user) throw NotFoundError(`No user found with id: ${_id}`);
+
+  if (!(await user.comparePassword(oldPassword))) {
+    res.clearCookie('token');
+    throw UnauthorizedError('Wrong password');
+  }
+
+  user.password = newPassword;
+  await user.save();
+
+  res.status(StatusCodes.OK).json({ msg: 'Password changed successfulyy' });
 };
