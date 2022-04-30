b8fa02b9ce4a403f11bfb63006cae655ced39ca7
Abhishek Singh <abhisheksinghxix@gmail.com>
2022-04-30 21:00:21 +0530

    Implement getuser route

--- src/controllers/user/getUser.ts
+++ src/controllers/user/getUser.ts
@@ -1,5 +1,12 @@
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { BadRequestError } from '../../errors';
+import { User } from '../../models';
 
-export const getUser = async (req: Request, res: Response) => {
-  res.json({ msg: 'getUser' });
+export const getUser = async (req: Request | any, res: Response | any) => {
+  const user = await User.findOne(
+    { _id: req.user._id },
+    { password: 0, isEmailVerified: 0, isPhoneVerified: 0, __v: 0 }
+  );
+  res.status(StatusCodes.OK).json({ user });
 };
