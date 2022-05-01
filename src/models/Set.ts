02e1086b37bcf06a601e42dc7de2904b6e48bf02
Abhishek Singh <abhisheksinghxix@gmail.com>
2022-05-01 19:18:28 +0530

    Add Set Model

---
+++ src/models/Set.ts
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const SetSchema = new mongoose.Schema({
+  liftID: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+    ref: 'Lift',
+  },
+  numReps: {
+    type: Number,
+    required: true,
+    validate: {
+      validator: function (v: number) {
+        return Number.isInteger(v) && v > 0;
+      },
+      message: '{VALUE} is not an integer value',
+    },
+  },
+  weight: {
+    type: Number,
+    required: true,
+    validate: {
+      validator: function (v: number) {
+        return v > 0;
+      },
+      message: '{VALUE} should be positive and non zero',
+    },
+  },
+});
+
+export const Set = mongoose.model('Set', SetSchema);
