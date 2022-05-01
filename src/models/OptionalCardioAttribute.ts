aa6fe022ddb6f95d1f47e70bf6bbeaed99e42755
Abhishek Singh <abhisheksinghxix@gmail.com>
2022-05-01 19:27:28 +0530

    Add OptionalCardioAttribute model

---
+++ src/models/OptionalCardioAttribute.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const OptionalCardioAttributeSchema = new mongoose.Schema({
+  cardioSessionID: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+    ref: 'CardioSession',
+  },
+  key: {
+    type: String,
+    minlength: 1,
+    maxlength: 30,
+    required: true,
+  },
+  value: {
+    type: String,
+    minlength: 1,
+    maxlength: 30,
+    required: true,
+  },
+});
+
+export const OptionalCardioAttribute = mongoose.model(
+  'OptionalCardioAttribute',
+  OptionalCardioAttributeSchema
+);
