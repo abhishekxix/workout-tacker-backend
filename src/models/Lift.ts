e140129b92eb5b8b9886fa191a947c7cc8ebef1d
Abhishek Singh <abhisheksinghxix@gmail.com>
2022-05-01 19:05:33 +0530

    Add Lift model

---
+++ src/models/Lift.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const LiftSchema = new mongoose.Schema({
+  workoutSessionID: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+    ref: 'WorkoutSession',
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 50,
+  },
+  type: {
+    type: String,
+    required: true,
+    enum: ['bodyweight', 'dumbbell', 'barbell', 'kettlebell', 'cable', 'other'],
+  },
+});
+
+LiftSchema.post('remove', async function () {
+  // Delete associated documents
+});
+
+export const Lift = mongoose.model('Lift', LiftSchema);
