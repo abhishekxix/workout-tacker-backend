1dfdcf20e2431f9f1e58eda2a98073c23a6503d2
Abhishek Singh <abhisheksinghxix@gmail.com>
2022-05-01 18:59:31 +0530

    Add WorkoutSessionModel

---
+++ src/models/WorkoutSession.ts
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const WorkoutSessionSchema = new mongoose.Schema({
+  day: {
+    type: mongoose.Types.ObjectId,
+    ref: 'Day',
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 50,
+  },
+  startTime: {
+    type: Date,
+    required: true,
+  },
+  endTime: {
+    type: Date,
+    required: false,
+  },
+});
+
+WorkoutSessionSchema.post('remove', async function () {
+  // Delete associated documents
+});
+
+export const WorkoutSession = mongoose.model(
+  'WorkoutSession',
+  WorkoutSessionSchema
+);
