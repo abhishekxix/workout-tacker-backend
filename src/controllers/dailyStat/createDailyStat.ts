b9a7be3a2db0315a174cea566ee468fcace803c2
Abhishek Singh <abhisheksinghxix@gmail.com>
2022-05-03 20:58:06 +0530

    Implement createDailyStat route

--- src/controllers/dailyStat/createDailyStat.ts
+++ src/controllers/dailyStat/createDailyStat.ts
@@ -1,5 +1,26 @@
 import {Request, Response} from 'express';
+import {StatusCodes} from 'http-status-codes';
+import {DailyStat} from '../../models';
 
 export const createDailyStat = async (req: Request, res: Response) => {
-  res.end('createDailyStat');
+  const {date, weight} = req.body;
+  const dateObject = new Date(date);
+
+  // eslint-disable-next-line max-len
+  const dateString = `${dateObject.getUTCFullYear()}-${dateObject.getUTCMonth()+1}-${dateObject.getUTCDate()}`;
+  // ^ The one is added because the month is 0 indexed
+
+  const userID = res.locals.user._id;
+
+  const dailyStat = await DailyStat.create({
+    date: dateString,
+    weight,
+    userID,
+  });
+  res.status(StatusCodes.CREATED)
+      .json({
+        _id: dailyStat._id,
+        date: dailyStat.date,
+        userID: dailyStat.userID,
+      });
 };
