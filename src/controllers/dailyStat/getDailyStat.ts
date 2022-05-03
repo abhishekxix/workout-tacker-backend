0d00fc11b33816ace98563e0c184a4b44ec8a3a3
Abhishek Singh <abhisheksinghxix@gmail.com>
2022-05-03 22:28:18 +0530

    Implement the getDailyStat route controller

--- src/controllers/dailyStat/getDailyStat.ts
+++ src/controllers/dailyStat/getDailyStat.ts
@@ -1,5 +1,20 @@
 import {Request, Response} from 'express';
+import {StatusCodes} from 'http-status-codes';
+import {NotFoundError} from '../../errors';
+import {DailyStat} from '../../models';
 
 export const getDailyStat = async (req: Request, res: Response) => {
-  res.end('getDailyStat');
+  const {_id} = req.body;
+
+  let result;
+
+  if (!_id) {
+    result = await DailyStat.find({}, {__v: 0}) ?? [];
+  } else {
+    result = await DailyStat.findById(_id, {__v: 0});
+    if (!result) throw new NotFoundError(`No dailyStat found with _id ${_id}`);
+  }
+
+
+  res.status(StatusCodes.OK).json(result);
 };
